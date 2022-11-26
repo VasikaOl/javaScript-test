b0aa7591083edee8095f20769987ea043c54a393
Vasyl Oliinyk <vasik.oliinyk@gmail.com>
2022-11-26 14:40:02 +0200

    add

--- js/module-2-lessons-3.js
+++ js/module-2-lessons-3.js
@@ -117,6 +117,41 @@
 
 // console.log(numbers);
 
-const numbers = [0, 5, 10, ...[1, 2, 3], 4, 5]
+// const numbers = [0, 5, 10, ...[1, 2, 3], 4, 5]
 
-console.log(numbers);
+// console.log(numbers);
+
+// const temps = [18, 14, 12, 21, 17, 29, 24, 1000, 1]
+
+// console.log(Math.max(...temps));
+// console.log(Math.min(...temps));
+
+// const doMath = function(a, b, callback) {
+//   const result = callback(a, b);
+
+//   console.log(result);
+// }
+
+// const add = function (x, y) {
+//   return x + y;
+// };
+
+// const sub = function (x, y) {
+//   return x - y;
+// };
+
+
+// doMath(2, 3, add);
+// doMath(10, 8, sub);
+
+// doMath(2, 3, function (x, y) {
+//   return x + y;
+// }
+// );
+// doMath(10, 8, function (x, y) {
+//   return x - y;
+// }
+// );
+
+const getSortedFriends = users => [...users].sort((a, b) => a.friends.length.localeCompare(b.friends.length)).filter((element, index, array) => array.indexOf(element) === index).flatMap(user => user.friends);
+console.log(users);
