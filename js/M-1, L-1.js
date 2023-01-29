1f52b4c8f03c8b5601035e8de0df2dd915df6d43
Vasyl Oliinyk <vasik.oliinyk@gmail.com>
2023-01-29 23:03:12 +0200

    update

---
+++ js/M-1, L-1.js
@@ -0,0 +1,29 @@
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = `Chelsy`;
+// const message = `Ласкаво просимо`;
+// const isOpen = true;
+// const shouldConfirm = false;
+
+// console.log(`імя`, userName, message);
+
+
+// const type = typeof 5;
+// console.log(type);
+
+
+// console.log(`До`);
+// alert(`Можна`);
+// console.log(`Після`);
+
+// const shouldRenew = confirm(`Бажаєте продовжити підписку?`);
+// console.log(shouldRenew);
+
+// const quantity = prompt(`Введіть кількість товарів`);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+let quantity = prompt(`Введіть кількість товарів`);
+quantity = Number(quantity);
+console.log(quantity);
+console.log(typeof quantity);
